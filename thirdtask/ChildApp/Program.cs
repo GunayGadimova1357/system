44adcceab94cde623f40b39723b0663ac11df658
Gunay <gadimovagunay87@gmail.com>
2025-05-06 11:48:30 +0400

    third

---
+++ thirdtask/ChildApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 3)
+        {
+            Console.WriteLine("not enough arguments.");
+            Environment.Exit(1);
+            return;
+        }
+
+        int a = int.Parse(args[0]);
+        int b = int.Parse(args[1]);
+        string op = args[2];
+
+        int result = 0;
+
+        switch (op)
+        {
+            case "+": result = a + b; break;
+            case "-": result = a - b; break;
+            case "*": result = a * b; break;
+            case "/": result = b != 0 ? a / b : 0; break;
+            default:
+                Console.WriteLine("unknown operator.");
+                Environment.Exit(2);
+                return;
+        }
+
+        Console.WriteLine(result);
+        Environment.Exit(0);
+    }
+}
---
+++ thirdtask/thirdtask/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("first num: ");
+            string firstNum = Console.ReadLine();
+            Console.WriteLine("second num: ");
+            string secondNum = Console.ReadLine();
+            Console.WriteLine("op(+;-;*;/: ");
+            string op = Console.ReadLine();
+            string args = $"{firstNum} {secondNum} {op}";
+            Process process = new Process();
+            process.StartInfo.FileName = "/Users/xalida/RiderProjects/thirdtask/ChildApp/bin/Debug/net8.0/ChildApp";
+            process.StartInfo.Arguments = args;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            Console.WriteLine($"result: {result}");
+            Console.WriteLine($"exit code: {process.ExitCode}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("error: " + ex.Message);
+        }
+
+ 
+    }
+}
