9bea38b56197a7c07416b6e72911e26cc2547132
Gunay <gadimovagunay87@gmail.com>
2025-05-06 11:49:49 +0400

    fourth

---
+++ fourthtask/ChildApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Недостаточно аргументов.");
+            Environment.Exit(1);
+            return;
+        }
+
+        string path = args[0];
+        string word = args[1];
+
+        if (!File.Exists(path))
+        {
+            Console.WriteLine("Файл не найден.");
+            Environment.Exit(2);
+            return;
+        }
+
+        string content = File.ReadAllText(path);
+        int count = content.Split(new[] { word }, StringSplitOptions.None).Length - 1;
+
+        Console.WriteLine(count);
+        Environment.Exit(0);
+    }
+}
---
+++ fourthtask/fourthtask/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            /// bicycle is fast. I have a bicycle. My friend has a bicycle too.
+            /// bicycle 
+            Console.WriteLine("file path: ");
+            string filePath = Console.ReadLine();
+            
+            Console.WriteLine("word to find: ");
+            string word = Console.ReadLine();
+            
+            string args = $"{filePath} {word}";
+            
+            Process process = new Process();
+            process.StartInfo.FileName = "/Users/xalida/RiderProjects/fourthtask/ChildApp/bin/Debug/net8.0/ChildApp";
+            process.StartInfo.Arguments = args;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            Console.WriteLine($"result: {result.Trim()} times");
+            Console.WriteLine($"exit code: {process.ExitCode}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("error: " + ex.Message);
+        }
+
+ 
+    }
+}
